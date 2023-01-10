ef818a63b15629bc7bb4b08a043c3e566e7509a6
hankehly <henry.ehly@gmail.com>
2023-01-11 07:55:38 +0900

    Replace static JS/CSS files with most recent CDN links

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// divide screen into 9 squares
+// Top 3 - up face
+// Left middle - left face
+// Right middle - right face
+// Bottom 3 - down face
+// window.onpointermove = function (ev) {
+//     console.log(ev);
+// };
