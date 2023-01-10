076a41088ffb5a0dcce2990de3f292441ffdf60b
hankehly <henry.ehly@gmail.com>
2023-01-11 08:08:29 +0900

    Update navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// divide screen into 9 squares
-// Top 3 - up face
-// Left middle - left face
-// Right middle - right face
-// Bottom 3 - down face
+// divide screen into 9 squares:
+//  up    up    up
+// left  none  right
+// left  none  right
+
 // window.onpointermove = function (ev) {
 //     console.log(ev);
 // };
